9892191a0b1ae8c07905f31071f3b2187c778afa
Md Amman <md.krazio@gmail.com>
2026-01-27 22:43:54 +0530

    redefine color scheme for login page

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,10 +1,10 @@
-import "./login.css";
 import { useState, useRef } from 'react';
-import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { CircularProgress } from '@mui/material';
+import './login.css';
 
 const Login = () => {
     const email = useRef();
@@ -35,45 +35,43 @@ const Login = () => {
                 localStorage.setItem('user', JSON.stringify(response.data));
                 toast.success('Login successful!');
                 navigate('/home');
-
-            }
-            else
-                toast.error('Invalid credentials!');
-        }
-        catch (err) {
-            if (err.response && err.response.data && err.response.data.Error)
-                toast.error(err.response.data.Error);
+            } else toast.error('Invalid credentials!');
+        } catch (err) {
+            if (err.response && err.response.data && err.response.data.Error) toast.error(err.response.data.Error);
             setLoading(false);
         }
     };
 
     return (
-        <div className='containerLogin'>
+        <div className="containerLogin">
             <div className="login-div">
-
                 <div className="loginCircle lOne"></div>
                 <div className="loginCircle lTwo"></div>
                 <div className="loginShadow"></div>
 
                 <div className="loginBox">
                     <div className="logo">
-                        <Link to={'/'}><label>WhisperWave</label></Link>
+                        <Link to={'/'}>
+                            <label>WhisperWave</label>
+                        </Link>
                     </div>
-                    <span className="title">Login</span>
+                    <span className="title">Welcome Back!</span>
 
-                    <form onSubmit={handleSubmit} className='loginForm'>
-                        <input className='loginInput' type="email" ref={email} placeholder='Email' />
-                        <input className='loginInput' type="password" ref={password} placeholder='Password' />
-                        <button disabled={loading} type="submit" className='customLoginBtn loginBtn'>
+                    <form onSubmit={handleSubmit} className="loginForm">
+                        <input className="loginInput" type="email" ref={email} placeholder="Email" />
+                        <input className="loginInput" type="password" ref={password} placeholder="Password" />
+                        <button disabled={loading} type="submit" className="customLoginBtn loginBtn">
                             {loading ? <CircularProgress size={28} color="inherit" /> : <span>Login</span>}
                         </button>
                     </form>
 
-                    <p>Don't have an account? &nbsp; <Link to="/register">Register</Link></p>
+                    <p>
+                        Don't have an account? &nbsp; <Link to="/register">Register</Link>
+                    </p>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Login
+export default Login;
