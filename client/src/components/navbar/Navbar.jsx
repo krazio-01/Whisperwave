0d017f4b961a1908fb92ca4c4d123eb6719c9d0b
Md Amman <md.krazio@gmail.com>
2026-01-28 23:12:13 +0530

    update navbar section

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,49 +1,49 @@
-import './navbar.css';
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
 import logo from '../../Assets/images/logo.png';
 import menu from '../../Assets/images/menu.png';
-import user from '../../Assets/images/user.png';
+import { IoLogIn } from "react-icons/io5";
+import './navbar.css';
 
 const Navbar = () => {
 
-  const [showMenuItems, setShowMenuItems] = useState(false);
+    const [showMenuItems, setShowMenuItems] = useState(false);
 
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
-  const handleSignInClick = () => {
-    navigate("/register");
-  };
+    const handleSignInClick = () => {
+        navigate("/login");
+    };
 
-  const handleMenuItemClick = () => {
-    setShowMenuItems(false);
-  };
+    const handleMenuItemClick = () => {
+        setShowMenuItems(false);
+    };
 
-  return (
-    <nav className='main-nav'>
-      <div className="logo">
-        <div className="hamburger">
-          <img src={menu} alt='Menu' onClick={() => setShowMenuItems(!showMenuItems)} />
-        </div>
+    return (
+        <nav className='main-nav'>
+            <div className="logo">
+                <div className="hamburger">
+                    <img src={menu} alt='Menu' onClick={() => setShowMenuItems(!showMenuItems)} />
+                </div>
 
-        <img className='webLogo' src={logo} alt='Logo' />
-        <label id='whisper'>WhisperWave</label>
-      </div>
+                <img className='webLogo' src={logo} alt='Logo' />
+                <label id='whisper'>WhisperWave</label>
+            </div>
 
-      <ul className={"menu-links" + (showMenuItems ? " active" : "")}>
-        <li><Link to='/' onClick={handleMenuItemClick}>Home</Link></li>
-        <li><Link to='/about' onClick={handleMenuItemClick}>About</Link></li>
-        <li><Link to='/contact' onClick={handleMenuItemClick}>Contact</Link></li>
-      </ul>
+            <ul className={"menu-links" + (showMenuItems ? " active" : "")}>
+                <li><Link to='/' onClick={handleMenuItemClick}>Home</Link></li>
+                <li><Link to='/about' onClick={handleMenuItemClick}>About</Link></li>
+                <li><Link to='/contact' onClick={handleMenuItemClick}>Contact</Link></li>
+            </ul>
 
-      <div className="btn">
-        <button onClick={handleSignInClick}>
-          <img src={user} alt='Login' /> SignIn
-        </button>
-      </div>
-    </nav>
-  );
+            <div className="btn">
+                <button onClick={handleSignInClick}>
+                    <IoLogIn /> SignIn
+                </button>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
