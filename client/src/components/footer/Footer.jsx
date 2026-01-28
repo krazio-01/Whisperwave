3869806b890ee4031fcd06482e44ecb2f2e36512
Md Amman <md.krazio@gmail.com>
2026-01-28 23:10:14 +0530

    update footer section and use react-icons

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,38 +1,31 @@
-import './footer.css';
-import React from 'react';
 import { Link } from 'react-router-dom';
-import linkedin from '../../Assets/images/linkedin.png';
-import github from '../../Assets/images/github.png';
-import twitter from '../../Assets/images/twitter.png';
+import { FaLinkedin, FaGithub, FaTwitter } from "react-icons/fa";
+import './footer.css';
 
 const Footer = () => {
     return (
-        <>
-            {/* <div className="wave2"></div> */}
-            <footer>
-                <div className="footer-content">
-                    <h3>WhisperWave</h3>
-                    <p>Never hesitate to share your ideas, we are here to help you</p>
+        <footer>
+            <div className="footer-content">
+                <h3>WhisperWave</h3>
+                <p>Never hesitate to share your ideas, we are here to help you</p>
 
-                    <ul className='socials'>
-                        <li>
-                            <Link to={'https://www.linkedin.com/in/md-amman/'}><img src={linkedin} alt="linkedin--v1" /></Link>
-                        </li>
-                        <li>
-                            <Link to={'https://github.com/krazio-01'}><img src={github} alt="github--v1" /></Link>
-                        </li>
-                        <li>
-                            <Link to={'https://twitter.com/krazio01'}><img src={twitter} alt="twitter--v1" /></Link>
-                        </li>
-                    </ul>
-                </div>
+                <ul className='socials'>
+                    <li>
+                        <Link to={'https://www.linkedin.com/in/md-amman/'}><FaLinkedin /></Link>
+                    </li>
+                    <li>
+                        <Link to={'https://github.com/krazio-01'}><FaGithub /></Link>
+                    </li>
+                    <li>
+                        <Link to={'https://twitter.com/krazio01'}><FaTwitter /></Link>
+                    </li>
+                </ul>
+            </div>
 
-                <div className="footer-bottom">
-                    <p>Copyright &copy; 2023 WhisperWave. Designed by <span>Md Amman</span></p>
-                </div>
-            </footer>
-
-        </>
+            <div className="footer-bottom">
+                <p>Copyright &copy; 2023 WhisperWave. Designed by <span>Md Amman</span></p>
+            </div>
+        </footer>
     )
 }
 
