75756291c4252033c74d1b742148e1ec51c62073
Md Amman <md.krazio@gmail.com>
2026-02-14 23:42:40 +0530

    update user model

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema(
     {
@@ -9,34 +9,27 @@ const UserSchema = new mongoose.Schema(
             max: 20,
             unique: true,
         },
+        lastUsernameChange: { type: Date, default: null },
         email: {
             type: String,
             required: true,
             max: 25,
             unique: true,
         },
+        tempEmail: { type: String, default: null },
         password: {
             type: String,
             required: true,
             min: 1,
         },
-        confirm_password: {
-            type: String,
-            min: 1,
-        },
-        isVerified: {
-            type: Boolean,
-            default: false,
-        },
-        emailToken: {
-            type: String,
-        },
-        profilePicture: {
-            type: String,
-            default: "",
-        },
+        confirm_password: { type: String, min: 1 },
+        isVerified: { type: Boolean, default: false },
+        emailToken: { type: String },
+        emailOtp: { type: String, default: null },
+        otpExpire: { type: Date, default: null },
+        profilePicture: { type: String, default: '' },
     },
-    { timestamps: true }
+    { timestamps: true },
 );
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model('User', UserSchema);
