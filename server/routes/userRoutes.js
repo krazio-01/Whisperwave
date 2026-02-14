177db54f02b9d7b617fa6a5c3887b31566701d53
Md Amman <md.krazio@gmail.com>
2026-02-14 23:44:57 +0530

    create update user profile and verify otp routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,26 @@
-const router = require("express").Router();
-const { searchUser, fetchAllAssociatedUsers } = require("../controllers/userControllers");
-const { protect } = require("../middlewares/authMiddleware");
+const router = require('express').Router();
+const {
+    searchUser,
+    fetchAllAssociatedUsers,
+    updateUserProfile,
+    verifyEmailChange,
+} = require('../controllers/userControllers');
+const { protect } = require('../middlewares/authMiddleware');
+const multer = require('multer');
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
 // search a user
-router.route("/searchUser").post(protect, searchUser);
+router.route('/searchUser').post(protect, searchUser);
 
 // get all users
-router.route("/associated").get(protect, fetchAllAssociatedUsers);
+router.route('/associated').get(protect, fetchAllAssociatedUsers);
+
+// update user details
+router.put('/update', protect, upload.single('profilePicture'), updateUserProfile);
+
+// verify email change
+router.post('/verify-email-change', protect, verifyEmailChange);
 
 module.exports = router;
