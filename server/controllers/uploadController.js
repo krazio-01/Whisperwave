780e608b3968003559d300705aedf12fa3da5438
Md Amman <md.krazio@gmail.com>
2026-02-15 14:04:06 +0530

    create image delete handlers

--- server/controllers/uploadController.js
+++ server/controllers/uploadController.js
@@ -4,9 +4,7 @@ const sharp = require('sharp');
 const uploadImageToCloudinary = async (file, folder) => {
     try {
         // Use sharp to compress the image
-        const compressedImageBuffer = await sharp(file.buffer)
-            .jpeg({ quality: 70 })
-            .toBuffer();
+        const compressedImageBuffer = await sharp(file.buffer).jpeg({ quality: 70 }).toBuffer();
 
         const result = await new Promise((resolve) => {
             const uploadStream = cloudinary.uploader.upload_stream(
@@ -17,7 +15,7 @@ const uploadImageToCloudinary = async (file, folder) => {
                 (error, result) => {
                     if (error) return;
                     else resolve(result);
-                }
+                },
             );
             uploadStream.end(compressedImageBuffer);
         });
@@ -29,4 +27,47 @@ const uploadImageToCloudinary = async (file, folder) => {
     }
 };
 
-module.exports = { uploadImageToCloudinary };
+const deleteImageFromCloudinary = async (imageUrl, folderName) => {
+    if (!imageUrl) return;
+
+    // default avatar check
+    if (imageUrl.includes('noAvatar_fr72mb.png')) return;
+
+    try {
+        const urlParts = imageUrl.split('/');
+        const fileName = urlParts[urlParts.length - 1].split('.')[0];
+
+        const publicId = `whisperwave/${folderName}/${fileName}`;
+
+        await cloudinary.uploader.destroy(publicId);
+    } catch (error) {
+        console.error(`Failed to delete image (${imageUrl}):`, error);
+    }
+};
+
+const deleteManyImages = async (imageUrls, folderName) => {
+    if (!imageUrls || imageUrls.length === 0) return;
+
+    const publicIds = imageUrls
+        .map((url) => {
+            const urlParts = url.split('/');
+            const fileName = urlParts[urlParts.length - 1].split('.')[0];
+            return `whisperwave/${folderName}/${fileName}`;
+        })
+        .filter((id) => id !== null);
+
+    if (publicIds.length === 0) return;
+
+    try {
+        const CHUNK_SIZE = 100;
+
+        for (let i = 0; i < publicIds.length; i += CHUNK_SIZE) {
+            const batch = publicIds.slice(i, i + CHUNK_SIZE);
+            await cloudinary.api.delete_resources(batch);
+        }
+    } catch (error) {
+        console.error('Failed to delete bulk images:', error);
+    }
+};
+
+module.exports = { uploadImageToCloudinary, deleteImageFromCloudinary, deleteManyImages };
