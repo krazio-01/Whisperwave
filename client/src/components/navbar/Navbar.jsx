867c2d697e25794481d215337ec4f991a277c30c
Md Amman <md.krazio@gmail.com>
2026-02-18 22:49:43 +0530

    update ui for navbar and add glassmorphism effect

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,19 +1,18 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 import logo from '../../Assets/images/logo.png';
 import menu from '../../Assets/images/menu.png';
-import { IoLogIn } from "react-icons/io5";
+import { IoLogIn } from 'react-icons/io5';
 import './navbar.css';
 
 const Navbar = () => {
-
     const [showMenuItems, setShowMenuItems] = useState(false);
 
     const navigate = useNavigate();
 
     const handleSignInClick = () => {
-        navigate("/login");
+        navigate('/login');
     };
 
     const handleMenuItemClick = () => {
@@ -21,26 +20,40 @@ const Navbar = () => {
     };
 
     return (
-        <nav className='main-nav'>
-            <div className="logo">
-                <div className="hamburger">
-                    <img src={menu} alt='Menu' onClick={() => setShowMenuItems(!showMenuItems)} />
+        <nav className="main-nav">
+            <div className="nav-wrapper">
+                <div className="logo">
+                    <div className="hamburger">
+                        <img src={menu} alt="Menu" onClick={() => setShowMenuItems(!showMenuItems)} />
+                    </div>
+
+                    <img className="webLogo" src={logo} alt="Logo" />
+                    <label id="whisper">WhisperWave</label>
                 </div>
 
-                <img className='webLogo' src={logo} alt='Logo' />
-                <label id='whisper'>WhisperWave</label>
-            </div>
+                <ul className={'menu-links' + (showMenuItems ? ' active' : '')}>
+                    <li>
+                        <Link to="/" onClick={handleMenuItemClick}>
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/about" onClick={handleMenuItemClick}>
+                            About
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/contact" onClick={handleMenuItemClick}>
+                            Contact
+                        </Link>
+                    </li>
+                </ul>
 
-            <ul className={"menu-links" + (showMenuItems ? " active" : "")}>
-                <li><Link to='/' onClick={handleMenuItemClick}>Home</Link></li>
-                <li><Link to='/about' onClick={handleMenuItemClick}>About</Link></li>
-                <li><Link to='/contact' onClick={handleMenuItemClick}>Contact</Link></li>
-            </ul>
-
-            <div className="btn">
-                <button onClick={handleSignInClick}>
-                    <IoLogIn /> SignIn
-                </button>
+                <div className="btn">
+                    <button onClick={handleSignInClick}>
+                        <IoLogIn /> SignIn
+                    </button>
+                </div>
             </div>
         </nav>
     );
