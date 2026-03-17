89ec860be61b3e4f05e3688df1a0c00e3243dc46
Md Amman <md.krazio@gmail.com>
2026-03-17 23:04:50 +0530

    switch to Brevo SMTP to bypass Render port blocks

--- server/utils/sendMail.js
+++ server/utils/sendMail.js
@@ -1,30 +1,30 @@
 const nodemailer = require('nodemailer');
-const dotenv = require('dotenv');
-dotenv.config();
 
 const sendEmail = async (userEmail, subject, text, html) => {
     try {
         const transporter = nodemailer.createTransport({
-            service: process.env.SERVICE,
-            port: process.env.EMAIL_PORT,
-            secure: process.env.SECURE,
+            host: process.env.BREVO_HOST,
+            port: process.env.BREVO_PORT,
+            secure: false,
             auth: {
-                user: process.env.USER,
-                pass: process.env.PASSWORD,
+                user: process.env.BREVO_USER,
+                pass: process.env.BREVO_PASSWORD,
             },
         });
 
         const mail_configs = {
-            from: `"Whisperwave" <${process.env.USER}>`,
+            from: `"Whisperwave" <${process.env.MAIL_FROM}>`,
             to: userEmail,
             subject: subject,
-            text: text,
-            html: html,
         };
 
-        await transporter.sendMail(mail_configs);
-        return transporter;
-    } 
+        if (text) mail_configs.text = text;
+        if (html) mail_configs.html = html;
+
+        const info = await transporter.sendMail(mail_configs);
+
+        return info;
+    }
     catch (error) {
         console.error('Error sending email:', error);
         throw error;
