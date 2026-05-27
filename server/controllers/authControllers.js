7c1efe18e38efde01e828f137c98861f634602c5
Md Amman <md.krazio@gmail.com>
2026-05-27 13:32:06 +0530

    implement forgotPassword controller logic and create it's email template

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -116,4 +116,55 @@ const verifyEmail = async (req, res) => {
     }
 };
 
-module.exports = { registerUser, loginUser, verifyEmail };
+const forgotPassword = async (req, res) => {
+    const { email } = req.body;
+
+    const SUCCESS_MESSAGE = 'If this email is registered, a reset link has been sent.';
+
+    try {
+        const user = await User.findOne({ email });
+        if (!user) return res.status(200).json({ message: SUCCESS_MESSAGE });
+
+        const LinkTTL = 15 * 60 * 1000; // 15 minutes
+        if (user.resetPasswordExpire && user.resetPasswordExpire > Date.now()) {
+            const timeRemaining = user.resetPasswordExpire - Date.now();
+            const timePassed = LinkTTL - timeRemaining;
+
+            const cooldownPeriod = 2 * 60 * 1000; // 2 minutes
+
+            if (timePassed < cooldownPeriod) {
+                const waitTime = Math.ceil((cooldownPeriod - timePassed) / 1000);
+                return res.status(429).json({
+                    message: `Please wait ${waitTime} seconds before requesting another link.`,
+                });
+            }
+        }
+
+        const resetToken = crypto.randomBytes(32).toString('hex');
+
+        user.resetPasswordToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+        user.resetPasswordExpire = Date.now() + LinkTTL;
+
+        await user.save();
+
+        const resetUrl = `${process.env.FRONTEND_URL}/reset-password/${resetToken}`;
+
+        await sendEmail(user.email, 'Password Reset Request', 'passwordResetEmail.ejs', {
+            username: user.username,
+            resetUrl: resetUrl,
+        });
+
+        res.status(200).json({ message: SUCCESS_MESSAGE });
+    } catch (error) {
+        const user = await User.findOne({ email });
+        if (user) {
+            user.resetPasswordToken = undefined;
+            user.resetPasswordExpire = undefined;
+            await user.save();
+        }
+        console.error('Password reset email failed:', error);
+        res.status(500).json({ message: 'Email could not be sent' });
+    }
+};
+
+module.exports = { registerUser, loginUser, verifyEmail, forgotPassword };
